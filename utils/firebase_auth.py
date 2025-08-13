8df052631df9621e4668854b16371241724221dd
Javiier00 <avilesedwar42@gmail.com>
2025-08-12 23:38:18 -0600

    nuevos cambios

--- utils/firebase_auth.py
+++ utils/firebase_auth.py
@@ -1,12 +1,5 @@
-import os
-import json
-from firebase_admin import credentials, initialize_app
+import firebase_admin
+from firebase_admin import credentials
 
-firebase_config = os.getenv("FIREBASE_CREDENTIALS")
-
-if firebase_config:
-    cred_dict = json.loads(firebase_config)
-    cred = credentials.Certificate(cred_dict)
-    initialize_app(cred)
-else:
-    raise FileNotFoundError("No Firebase credentials found in environment variables")
+cred = credentials.Certificate("secrets/firebase_credentials.json")
+firebase_admin.initialize_app(cred)
